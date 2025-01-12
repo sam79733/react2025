446a2875e195a9c988a913dc6176ac0e99b39c76
Gyanendra Srivastav <gyasriva@publicisgroupe.net>
2025-01-12 21:43:32 +0530

    add valid change

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>This paragraph to create a valid change</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
