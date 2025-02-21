4b7f6fd865ee51591252a75dbea95d1067f0acc4
Gyanendra Srivastav <gyasriva@publicisgroupe.net>
2025-02-21 17:20:47 +0530

    add footer in app js file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './Footer';
 
 function Header() {
   return (
-    <div className="section-header">
-      <h2>Welcome to my first react APP</h2>
-    </div>
+    <header className="App-header">
+      <h3>Welcome to React JS 2025</h3>
+    </header>
   );
 }
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <header className="App-header">
+      <Header />
+      <div className="product-container">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,7 +23,8 @@ function App() {
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
-      </header>
+      </div>
+      <Footer />
     </div>
   );
 }
